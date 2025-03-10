d86f490626a4eb56b580a492d9a5b2cdaff45588
Barbara Geovana <barbara.g.n.silva@gmail.com>
2025-03-10 19:26:46 -0300

    Primeiro Commit

---
+++ mediaAritimetrica.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class mediaAritimetrica {
+   public static void main (String[] args){
+    double notaUm, notaDois, media;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a primeira nota: ");
+    notaUm = scanner.nextDouble();
+    System.out.println("Digite a segunda nota: ");
+    notaDois = scanner.nextDouble();
+
+    scanner.close();
+
+    media = (notaUm + notaDois) / 2;
+    System.out.printf("A média das notas é: %.2f\n", media);
+
+    if(media >= 7){
+        System.out.printf("O aluno está APROVADO");
+    }
+   } 
+}   
